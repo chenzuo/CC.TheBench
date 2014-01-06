0a64d5f87f28e4ec2252901e80f9cb89f34d4125
David Cumps <david@cumps.be>
2014-01-06 22:47:06 +0100

    Starting with Simple Authentication

---
+++ src/CC.TheBench.Frontend.Web/Security/AuthenticationCallbackProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace CC.TheBench.Frontend.Web.Security
+{
+    using Nancy;
+    using Nancy.SimpleAuthentication;
+
+    public class AuthenticationCallbackProvider : IAuthenticationCallbackProvider
+    {
+        public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
+        {
+            return nancyModule.View["AuthenticateCallback", model];
+        }
+
+        public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
