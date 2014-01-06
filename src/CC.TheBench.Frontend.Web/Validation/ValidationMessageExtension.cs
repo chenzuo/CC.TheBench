8586c2155b1f7d3c582eb77801b4109d49830381
David Cumps <david@cumps.be>
2014-01-06 15:59:59 +0100

    Formatting validation error

---
+++ src/CC.TheBench.Frontend.Web/Validation/ValidationMessageExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace CC.TheBench.Frontend.Web.Validation
+{
+    using Nancy.ViewEngines.Razor;
+
+    public static class ValidationMessageExtension
+    {
+        public static string ValidationMessage<T>(this HtmlHelpers<T> html, string validationFormat, string validationField)
+        {
+            const string fluentValidationMarker = "{PropertyName}";
+
+            return validationFormat.Contains(fluentValidationMarker) 
+                ? validationFormat.Replace(fluentValidationMarker, validationField) 
+                : string.Format(validationFormat, validationField);
+        }
+    }
+}
