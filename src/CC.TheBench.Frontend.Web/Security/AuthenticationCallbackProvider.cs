28f85f4ad0410b9314872a7eb7c7e69a81064df5
David Cumps <david@cumps.be>
2014-01-07 09:50:31 +0100

    Added some TODOs

--- src/CC.TheBench.Frontend.Web/Security/AuthenticationCallbackProvider.cs
+++ src/CC.TheBench.Frontend.Web/Security/AuthenticationCallbackProvider.cs
@@ -7,12 +7,22 @@
     {
         public dynamic Process(NancyModule nancyModule, AuthenticateCallbackData model)
         {
+            // TODO: Look at how Jabbr does it: https://gist.github.com/CumpsD/8296129
+            // need to check if ex is null, and then go on with ID + provider name
             return nancyModule.View["AuthenticateCallback", model];
         }
 
         public dynamic OnRedirectToAuthenticationProviderError(NancyModule nancyModule, string errorMessage)
         {
             throw new System.NotImplementedException();
+
+            //when we redirect
+            //we take the key/secret, pass info to google, get a token url
+            //then redirect to the login page using that information
+            //if that request is bung, we throw the OnRedirectToAuthenticationProviderError
+            //with the info
+            //once that stuff is working, it should never break
+            //unless you change something, or the provider changes something
         }
     }
 }
