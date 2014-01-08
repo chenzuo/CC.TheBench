acaf779ea821253fe6603443f6604d8a00133d29
David Cumps <david@cumps.be>
2014-01-08 22:27:38 +0100

    Added Migrations project

---
+++ src/CC.TheBench.Frontend.Migrations/CreateUserTable.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace CC.TheBench.Frontend.Migrations
+{
+    [Migration(201401082219)]
+    public class CreateUserTable : Migration
+    {
+        public override void Up()
+        {
+            
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
